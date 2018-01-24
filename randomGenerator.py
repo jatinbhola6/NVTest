a6cfc8f08083dcccacbae1bb81379ea27a154d55
Jatin Bhola <jatinbhola6@gmail.com>
2018-01-24 17:43:58 +0530

    fixed issue

--- randomGenerator.py
+++ randomGenerator.py
@@ -3,11 +3,11 @@ def generateRandom(max,highPrcnt):
     upper_array = list(range(max//2,max+1))
     lower_array = list(range(max//2))
     if int(str(time.time())[-2:]) < highPrcnt :
-        return upper_array[int(time.time())%len(upper_array)]
+        return upper_array[int(time.time()*10000000)%len(upper_array)]
     else:
-        return lower_array[int(time.time()) % len(lower_array)]
+        return lower_array[int(time.time()*10000000) % len(lower_array)]
 
 if __name__ == "__main__":
-    a = generateRandom(10,73)
-    print("random number generated is:" + a)
-
+    for i in range(0,10):
+        a = generateRandom(10,73)
+        print("random number generated is:" + str(a))
